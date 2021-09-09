a4dabc6172c410c11771c262f363a282be244cfb
JmbFountain <jmb@smx.de>
2021-09-09 10:18:21 +0200

    Schwäbisch-Schale-Manuskript hinzugefügt

---
+++ schwaebisch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+git config --local alias.uffmache init
+git config --local alias.kobiere clone
+git config --local alias.ziage pull
+git config --local alias.drzudo add
+git config --local alias.drigge push
+git config --local alias.pfusch push --force-with-lease
+git config --local alias.abzweige branch
+git config --local alias.ibergäbe commit
+git config --local alias.schdaple rebase
+git config --local alias.ondrscheide diff
+git config --local alias.zammdoe merge
+git config --local alias.uffhebe stash
+git config --local alias.momole tag
+git config --local alias.raushole checkout
+git config --local alias.tagebuach log
+git config --local alias.zuaschtand status
+git config --local alias.beschuldige blame
+git config --local alias.bade remote
+
+echo "alias git='seggl'" >> ~/.bashrc
